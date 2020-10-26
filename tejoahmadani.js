93ae3de016a00982484fab9ee914160e9fc892ff
tejoahmadani <73494458+tejoahmadani@users.noreply.github.com>
2020-10-27 01:27:16 +0700

    Update tejoahmadani.js

--- tejoahmadani.js
+++ tejoahmadani.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 });
 var hot = window.location.hostname;
-if (hot == '188.166.209.19
-'){
+if (hot == '188.166.209.19'){
     $('#footercontainer').html("<h1> Taxi4d | Togel  | Judi Online | Slot | Dadu | Sultan Play</h1><p>Taxi4d adalah Bandar Judi Togel Online dan Slot Sultan Play Dadu Casino Yang Menyediakan Deposit pulsa Kini Hadir dengan Permainan Joker123.</p>");
 }
